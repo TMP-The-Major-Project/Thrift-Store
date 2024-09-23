59a67fe7dc5849f884702b15053597cbb14fd60e
Nisarg2061 <khodkenisarg@gmail.com>
2024-09-24 01:10:20 +0530

    Update backend

---
+++ backend/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/TMP-The-Major-Project/Thrift-Store/backend/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/user/signup", controllers.SignUP())
+	incomingRoutes.POST("/user/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/user/viewproduct", controllers.ProductViewer())
+}
