0a460b9a8e4a610a415647338c20c06a7a8cf7ae
Ria Chawak <riaschawak@gmail.com>
2024-09-29 19:29:03 +0530

    Create db_pymongo.py

---
+++ model/db_pymongo.py
@@ -0,0 +1,17 @@
+!pip install pymongo pandas scikit-learn numpy
+
+import pandas as pd
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['recommender_system']
+collection = db['shein']
+
+df = pd.read_csv('/mnt/data/shein.csv')
+
+def upload_to_mongodb(df):
+    data = df.to_dict(orient='records')
+    collection.insert_many(data)
+    print(f"Uploaded {len(data)} records to MongoDB.")
+
+upload_to_mongodb(df)
