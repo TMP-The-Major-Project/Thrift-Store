4c4e7688fbe0e85696942ee2e882c39aa83a109b
Nisarg2061 <khodkenisarg@gmail.com>
2024-11-15 17:40:16 +0530

    fix fromating

--- frontend/src/Admin/AdminDash.jsx
+++ frontend/src/Admin/AdminDash.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./AdminDash.css"
 import ProductForm from "./ProductForm";
-const AdminDash = () => {
+const AdminDash = ({ user }) => {
 
   const [product, setProduct] = useState([{}])
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -39,49 +39,55 @@ const AdminDash = () => {
   }
 
   const onDelete = async (id) => {
-        try{
-            const options = {
-                method: "DELETE"
-            }
-            const response = await fetch(`http://127.0.0.1:3001/products/${id}`, options)
-            if(response.status == 200){
-                onUpdate()
-            }else{
-                console.error("Failed to delete")
-            }
-        } catch(error){
-            alert(error)
-        }
+    try {
+      const options = {
+        method: "DELETE"
+      }
+      const response = await fetch(`http://127.0.0.1:3001/products/${id}`, options)
+      if (response.status == 200) {
+        onUpdate()
+      } else {
+        console.error("Failed to delete")
+      }
+    } catch (error) {
+      alert(error)
     }
+  }
+
+  if (user !== "test") {
+    return <div className="admin-container">
+      <h2 className="admin-title">Unauthorized</h2>
+    </div>;
+  }
 
   return (
     <>
-    <div className="admin-container">
-    <h2 className="admin-title">Product List</h2>
-    <button onClick={openCreateModal}>Add New Product</button>
-            { isModalOpen && <div className="modal">
-                <div className="modal_content">
-                  <span className="close" onClick={closeModal}>&times;</span>
-                <ProductForm existingProduct={currentProduct} updateCallback={onUpdate}/>
-                </div>
+      <div className="admin-container">
+        <h2 className="admin-title">Product List</h2>
+        <button onClick={openCreateModal}>Add New Product</button>
+        {isModalOpen && <div className="modal">
+          <div className="modal_content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <ProductForm existingProduct={currentProduct} updateCallback={onUpdate} />
+          </div>
+        </div>
+        }
+        {product.map((item) => (
+          <div className="admin-item" key={item.id}>
+            <img className="admin-item-image" src={item.img} alt={item.title} />
+            <div className="admin-item-description">
+              <p className="admin-item-title">ID: {item.id}</p>
+              <p className="admin-item-title">Product Name: {item.title}</p>
+              <p className="admin-item-price">Orignal Price: {item.prevPrice}</p>
+              <p className="admin-item-price">Discount Price: ₹{item.newPrice}</p>
+              <div className="admin-item-actions">
+                <button className="remove-from-admin-button" onClick={() => openEditModal(item)}>Edit</button>
+                <button className="remove-from-admin-button" onClick={() => onDelete(item.id)}>Delete</button>
               </div>
-              }
-    {product.map((item) => (
-      <div className="admin-item" key={item.id}>
-      <img className="admin-item-image" src={item.img} alt={item.title} />
-      <div className="admin-item-description">
-      <p className="admin-item-title">ID: {item.id}</p>
-      <p className="admin-item-title">Product Name: {item.title}</p>
-      <p className="admin-item-price">Orignal Price: {item.prevPrice}</p>
-      <p className="admin-item-price">Discount Price: ₹{item.newPrice}</p>
-      <div className="admin-item-actions">
-      <button className="remove-from-admin-button" onClick={() => openEditModal(item)}>Edit</button>
-      <button className="remove-from-admin-button" onClick={() => onDelete(item.id)}>Delete</button>
+            </div>
+          </div>
+        ))}
       </div>
-      </div>
-      </div>
-    ))}
-    </div>
     </>
   );
 };
